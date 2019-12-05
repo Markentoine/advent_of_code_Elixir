1935b2693790b28a3dd6b5feed7b38510b23ccc0
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-12-05 14:56:55 +0100

    Refactoring.

--- day_two/program_alarm/lib/program_alarm/process.ex
+++ day_two/program_alarm/lib/program_alarm/process.ex
@@ -1,10 +1,14 @@
 defmodule ProgramAlarm.Process do
   def find_solution_one do
-    :array.get(0, start())
+    :array.get(0, start(12, 2))
   end
 
-  def start do
-    general_process(0, ProgramAlarm.RetrieveState.restore_state())
+  def find_solution_two(noun, verb) do
+    :array.get(0, start(noun, verb))
+  end
+
+  def start(noun, verb) do
+    general_process(0, ProgramAlarm.RetrieveState.restore_state(noun, verb))
   end
 
   def general_process(headpos \\ 0, state) do
@@ -17,10 +21,7 @@ defmodule ProgramAlarm.Process do
   defp process(99, _headpos, state), do: state
 
   defp process(1, headpos, state) do
-    pv1 = :array.get(headpos + 1, state)
-    pv2 = :array.get(headpos + 2, state)
-    v1 = :array.get(pv1, state)
-    v2 = :array.get(pv2, state)
+    {v1, v2} = get_values(headpos, state)
     mem_store = :array.get(headpos + 3, state)
     new_state = :array.set(mem_store, v1 + v2, state)
     new_headpos = headpos + 4
@@ -29,14 +30,19 @@ defmodule ProgramAlarm.Process do
   end
 
   defp process(2, headpos, state) do
-    pv1 = :array.get(headpos + 1, state)
-    pv2 = :array.get(headpos + 2, state)
-    v1 = :array.get(pv1, state)
-    v2 = :array.get(pv2, state)
+    {v1, v2} = get_values(headpos, state)
     mem_store = :array.get(headpos + 3, state)
     new_state = :array.set(mem_store, v1 * v2, state)
     new_headpos = headpos + 4
     new_head_value = :array.get(new_headpos, new_state)
     process(new_head_value, new_headpos, new_state)
+  end
+
+  defp get_values(headpos, state) do
+    pv1 = :array.get(headpos + 1, state)
+    pv2 = :array.get(headpos + 2, state)
+    v1 = :array.get(pv1, state)
+    v2 = :array.get(pv2, state)
+    {v1, v2}
   end
 end
--- day_two/program_alarm/lib/program_alarm/retrieve_state.ex
+++ day_two/program_alarm/lib/program_alarm/retrieve_state.ex
@@ -1,7 +1,7 @@
 defmodule ProgramAlarm.RetrieveState do
-  def restore_state do
+  def restore_state(noun, verb) do
     inputs = ProgramAlarm.FetchInputs.array_inputs()
-    new_inputs = :array.set(1, 12, inputs)
-    :array.set(2, 2, new_inputs)
+    new_inputs = :array.set(1, noun, inputs)
+    :array.set(2, verb, new_inputs)
   end
 end
